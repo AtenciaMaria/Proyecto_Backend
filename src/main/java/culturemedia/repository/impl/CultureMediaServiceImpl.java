7e4591389eb636c45d450a32afcdc253af3159ef
AtenciaMaria <mariaf.atencia@udea.edu.co>
2024-10-16 21:05:06 -0500

    Adding/correction CultureMedia structures

--- src/main/java/culturemedia/repository/impl/CultureMediaServiceImpl.java
+++ src/main/java/culturemedia/repository/impl/CultureMediaServiceImpl.java
@@ -1,21 +1,25 @@
 package culturemedia.repository.impl;
 
 import culturemedia.exception.DuracionNotValidException;
-import culturemedia.exception.VideoNotFountException;
 import culturemedia.model.Video;
 import culturemedia.model.View;
 import culturemedia.repository.CultureMediaService;
+import culturemedia.repository.VideoRepository;
+import culturemedia.repository.ViewsRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CultureMediaServiceImpl implements CultureMediaService {
-    private final List<Video> videos = new ArrayList<>();
-    private final List<View> views = new ArrayList<>();
+    private final VideoRepository videoRepository;
+    private final ViewsRepository viewsRepository;
 
+    public CultureMediaServiceImpl(VideoRepository videoRepository, ViewsRepository viewsRepository) {
+        this.videoRepository = videoRepository;
+        this.viewsRepository = viewsRepository;
+    }
     @Override
     public List<Video> listarTodos() {
-        return new ArrayList<>(videos);
+        return this.videoRepository.listarTodos();
     }
 
     @Override
@@ -23,20 +27,11 @@ public class CultureMediaServiceImpl implements CultureMediaService {
         if (video.duracion() <=0){
             throw new DuracionNotValidException(video.titulo(),video.duracion());
         }
-        videos.add(video);
-        return video;
+        return this.videoRepository.agregarVideo(video);
     }
 
     @Override
-    public View agregar(View view) throws VideoNotFountException {
-        Video encontrarVideo = videos.stream()
-                .filter(p -> p.codigo().equals(view.video().codigo()))
-                .findFirst()
-                .orElse(null);
-        if (encontrarVideo == null){
-            throw new VideoNotFountException(view.video().titulo());
-        }
-        views.add(view);
-        return view;
+    public View agregar(View view)  {
+        return this.viewsRepository.agregar(view);
     }
 }

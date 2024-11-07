832f4f5807c1a9b0e1c59015b0835879ee5d2bfd
AtenciaMaria <mariaf.atencia@udea.edu.co>
2024-11-06 20:49:44 -0500

    Mock correction in test cases

--- src/test/java/culturemedia/service/CultureMediaServiceTest.java
+++ src/test/java/culturemedia/service/CultureMediaServiceTest.java
@@ -1,6 +1,5 @@
 package culturemedia.service;
 
-import culturemedia.exception.DuracionNotValidException;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.repository.VideoRepository;
@@ -26,63 +25,79 @@ class CultureMediaServiceTest {
     @Mock
     private ViewsRepository viewsRepository;
 
-
     @BeforeEach
     void init(){
         MockitoAnnotations.initMocks(this);
         cultureMediaService = new CultureMediaServiceImpl(videoRepository , viewsRepository);
     }
 
-
     @Test
     void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundException {
-        doReturn(List.of(new Video("01", "Título 1", "----", 4.5),
-                new Video("02", "Título 2", "----", 5.5),
-                new Video("03", "Título 3", "----", 4.4),
-                new Video("04", "Título 4", "----", 3.5),
-                new Video("05", "Clic 5", "----", 5.7),
-                new Video("06", "Clic 6", "----", 5.1)))
-                .when(videoRepository).listarTodos();
-        List<Video> videos = cultureMediaService.listarTodos( );
+        mock_findAll_videos_returned_successfully();
+        List<Video> videos = cultureMediaService.listarTodos();
         assertFalse(videos.isEmpty());
     }
 
     @Test
     void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        doReturn(new ArrayList<>()).when(videoRepository).listarTodos();
+        mock_findAll_does_not_find_any_video_returned_successfully();
         assertThrows(VideoNotFoundException.class, () -> cultureMediaService.listarTodos());
     }
 
     @Test
     void when_FindByTitle_only_videos_which_contains_the_word_in_the_title_should_be_returned_successfully() throws VideoNotFoundException {
-        doReturn(List.of(new Video("05", "Clic 5", "----", 5.7),
-                new Video("06", "Clic 6", "----", 5.1)))
-                .when(videoRepository).buscar("Clic");
-
+        mock_FindByTitle_videos_returned_successfully();
         List<Video> videos = cultureMediaService.buscar( "Clic" );
         assertFalse(videos.isEmpty());
     }
 
     @Test
     void when_FindByTitle_does_not_match_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        doReturn(new ArrayList<>()).when(videoRepository).buscar("No coincide");
+        mock_FindByTitle_does_not_find_any_videos_returned_successfully();
         assertThrows(VideoNotFoundException.class, () -> cultureMediaService.buscar("No coincide"));
     }
 
     @Test
     void when_FindByDuration_only_videos_between_the_range_should_be_returned_successfully() throws VideoNotFoundException {
-        doReturn(List.of(new Video("01", "Título 1", "----", 4.5),
-                new Video("02", "Título 2", "----", 5.5),
-                new Video("05", "Clic 5", "----", 5.7),
-                new Video("06", "Clic 6", "----", 5.1)))
-                .when(videoRepository).buscar(4.5, 5.7 );
+        mock_FindByDuration_videos_returned_successfully();
         List<Video> videos = cultureMediaService.buscar( 4.5, 5.7 );
         assertFalse(videos.isEmpty());
     }
 
     @Test
     void when_FindByDuration_does_not_match_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        doReturn(new ArrayList<>()).when(videoRepository).buscar(7.0,8.0);
+        mock_FindByDuration_does__not_find_any_videos_returned_successfully();
         assertThrows(VideoNotFoundException.class, () -> cultureMediaService.buscar(7.0,8.0) );
+    }
+
+    private void mock_findAll_videos_returned_successfully(){
+        doReturn(List.of(new Video("01", "Título 1", "----", 4.5),
+            new Video("02", "Título 2", "----", 5.5),
+            new Video("03", "Título 3", "----", 4.4),
+            new Video("04", "Título 4", "----", 3.5),
+            new Video("05", "Clic 5", "----", 5.7),
+            new Video("06", "Clic 6", "----", 5.1)))
+            .when(videoRepository).listarTodos();
+    }
+    private void mock_findAll_does_not_find_any_video_returned_successfully(){
+        doReturn(new ArrayList<>()).when(videoRepository).listarTodos();
+    }
+    private void mock_FindByTitle_videos_returned_successfully(){
+        doReturn(List.of(new Video("05", "Clic 5", "----", 5.7),
+                new Video("06", "Clic 6", "----", 5.1)))
+                .when(videoRepository).buscar("Clic");
+    }
+    private void mock_FindByTitle_does_not_find_any_videos_returned_successfully(){
+        doReturn(new ArrayList<>()).when(videoRepository).buscar("No coincide");
+    }
+    private void mock_FindByDuration_videos_returned_successfully(){
+        doReturn(List.of(new Video("01", "Título 1", "----", 4.5),
+                new Video("02", "Título 2", "----", 5.5),
+                new Video("05", "Clic 5", "----", 5.7),
+                new Video("06", "Clic 6", "----", 5.1)))
+                .when(videoRepository).buscar(4.5, 5.7 );
+    }
+    private void mock_FindByDuration_does__not_find_any_videos_returned_successfully(){
+        doReturn(new ArrayList<>()).when(videoRepository).buscar(7.0,8.0);
     }
 }
